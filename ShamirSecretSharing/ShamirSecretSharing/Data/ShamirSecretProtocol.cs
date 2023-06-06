425017440b2c61d813a8c8299fff715eb720e83c
Andrey <84859114+Andrey1039@users.noreply.github.com>
2023-06-06 23:27:25 +0500

    Добавлена программа

---
+++ ShamirSecretSharing/ShamirSecretSharing/Data/ShamirSecretProtocol.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace ShamirSecretSharing.Data
+{
+    internal static class ShamirSecretProtocol
+    {
+        // Формирование частей секрета
+        public static List<Tuple<int, BigInteger>> ShareSecret(BigInteger secret, int threshold, int numShares, BigInteger prime)
+        {
+            List<Tuple<int, BigInteger>> shares = new List<Tuple<int, BigInteger>>();
+
+            BigInteger[] coefficients = new BigInteger[numShares];
+            coefficients[0] = secret;
+
+            for (int i = 1; i < numShares; i++)
+                coefficients[i] = GenerateRandomNumber(prime);
+
+            for (int i = 0; i < numShares; i++)
+            {
+                BigInteger x = new BigInteger(i + 1);
+                BigInteger y = 0;
+
+                for (int j = 0; j < threshold; j++)
+                    y += coefficients[j] * BigInteger.Pow(x, j) % prime;
+
+                shares.Add(new Tuple<int, BigInteger>(i + 1, y));
+            }
+
+            return shares;
+        }
+
+        // Генерация случайных чисел
+        private static BigInteger GenerateRandomNumber(BigInteger max)
+        {
+            byte[] bytes = max.ToByteArray();
+            BigInteger number;
+
+            using (RandomNumberGenerator rand = RandomNumberGenerator.Create())
+            {
+                do
+                {
+                    rand.GetBytes(bytes);
+                    bytes[bytes.Length - 1] &= (byte)0x7F;
+                    number = new BigInteger(bytes);
+                }
+                while (number >= max && number == 0);
+            }
+
+            return number;
+        }
+
+        // Восстановление секрета
+        public static BigInteger RecoverSecret(List<Tuple<int, BigInteger>> shares, BigInteger prime, int threshold)
+        {
+            BigInteger secret = 0;
+
+            for (int i = 0; i < threshold; i++)
+            {
+                BigInteger xi = shares[i].Item1;
+                BigInteger yi = shares[i].Item2;
+
+                BigInteger numerator = 1;
+                BigInteger denominator = 1;
+
+                for (int j = 0; j < threshold; j++)
+                {
+                    if (i != j)
+                    {
+                        BigInteger xj = shares[j].Item1;
+
+                        numerator *= -xj;
+                        denominator *= (xi - xj);
+                    }
+                }
+
+                BigInteger inverseDenominator = InverseModule(denominator, prime);
+                secret += yi * numerator * inverseDenominator;
+            }
+
+            secret %= prime;
+
+            return secret < 0 ? secret + prime : secret;
+        }
+
+        // Расширенный алгоритм Евклида
+        private static BigInteger InverseModule(BigInteger a, BigInteger m)
+        {
+            if (a < 0) a += m;
+
+            if (m == 1)
+                return 0;
+
+            BigInteger m0 = m;
+            BigInteger t, q;
+            BigInteger x0 = 0, x1 = 1;
+
+            while (a > 1)
+            {
+                q = a / m;
+                t = m;
+                m = a % m;
+                a = t;
+                t = x0;
+                x0 = x1 - q * x0;
+                x1 = t;
+            }
+
+            if (x1 < 0)
+                x1 += m0;
+
+            return x1;
+        }
+    }
+}
---
+++ ShamirSecretSharing/ShamirSecretSharing/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Numerics;
+using ShamirSecretSharing.Data;
+using System.Windows.Documents;
+using System.Collections.Generic;
+
+namespace ShamirSecretSharing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        // Разбиение секрета на части
+        private string CreateSecretParts(string secret, int threshold, int numShares, BigInteger prime)
+        {
+            BigInteger bigSecret = new BigInteger(Encoding.GetEncoding(1251).GetBytes(secret));
+            List<Tuple<int, BigInteger>> shares = ShamirSecretProtocol.ShareSecret(bigSecret, threshold, numShares, prime);
+
+            SecretPartsTB.Document.Blocks.Clear();
+            string result = string.Empty;
+
+            foreach (Tuple<int, BigInteger> share in shares)
+            {
+                result += $"{share.Item1}:{share.Item2}\r";
+            }
+
+            return result;
+        }
+
+        // Восстановление секрета
+        private string RecoverSecret(string[] secretParts, int numShares, BigInteger prime)
+        {
+            List<Tuple<int, BigInteger>> shares = new List<Tuple<int, BigInteger>>();
+
+            Random rand = new Random();
+            int[] randNumbers = Enumerable.Range(0, secretParts.Length)
+                .OrderBy(x => rand.Next())
+                .Take(numShares)
+                .ToArray();
+
+            for (int i = 0; i < numShares; i++)
+            {
+                string[] inputText = secretParts[randNumbers[i]].Trim().Split(':');
+
+                int num = int.Parse(inputText[0]);
+                BigInteger partSecret = BigInteger.Parse(inputText[1]);
+
+                shares.Add(new Tuple<int, BigInteger>(num, partSecret));
+            }
+
+            BigInteger recoveredSecret = ShamirSecretProtocol.RecoverSecret(shares, prime, numShares);
+            byte[] byteText = recoveredSecret.ToByteArray();
+            string secret = Encoding.GetEncoding(1251).GetString(byteText);
+
+            return secret;
+        }
+
+        // Работа с протоколом
+        private void ShamirShare()
+        {
+            // threshold <= numShares
+            // secret < prime
+
+            string secret = OriginalSecretTB.Text;
+            int threshold = int.Parse(ThresholdTB.Text);
+            int numShares = int.Parse(NumShareTB.Text);
+            BigInteger prime = BigInteger.Parse("1949117474374491121499531154375925322401162097726698633624512766087028063654507242883388729918194413");
+
+            string shares = CreateSecretParts(secret, threshold, numShares, prime);
+            SecretPartsTB.AppendText(shares);
+
+            string[] secretParts = new TextRange(
+                SecretPartsTB.Document.ContentStart,
+                SecretPartsTB.Document.ContentEnd
+            ).Text.Replace("\r", " ").Replace("\n", "").Split(" ").Where(x => x != "").ToArray();
+
+            ReceivedSecretTB.Text = RecoverSecret(secretParts, int.Parse(NewNumShareTB.Text), prime);
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ShamirShare();
+        }
+    }
+}
